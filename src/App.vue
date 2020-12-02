6fa6a9522b22ed63399067113a85a4c7ec5c1021
zoe-cy <1429567083@qq.com>
2020-12-02 21:41:17 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    index
+    <vheader></vheader>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {}
-    },
-    created: function () {
-      this.$router.push({path: '/inTheaters'})
-    }
-  }
+import vheader from './components/header/header'
+export default {
+     components: {
+       vheader
+     }
+}
 </script>
 
 <style>
-  #app {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    overflow: scroll;
-    font-family: 'FontAwesome', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: #E5E9F2;
-    color: #2c3e50;
-  }
+ 
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,99 +1,26 @@
 <template>
-  <div>
-    <div class="v-header">
-      <div class="v-header-logo">
-        <div>
+   <div>
+      <div class="header">
           <img src="../../assets/logo.png">
-        </div>
-        <div class="v-load-title">
-          <h3>狗眼电影</h3>
-          <div>查影讯，上狗眼电影就够了！</div>
-        </div>
+          <div>
+            <h3>狗眼电影</h3>
+            <p>一手资讯，上狗眼电影就够了！</p>
+            <input type="text" placeholder="请输入电影名" autofocus>
+          </div>
       </div>
-      <div class="v-header-input" @click="serch">
-        <input class="v-inp" type="text" v-model.trim="query" placeholder="请输入电影名"></input>
+      <div class="nav">
+          <a href="#">正在热映</a>
+          <a href="#">即将上映</a>
       </div>
-    </div>
-    <div class="v-nav-bar">
-      <router-link to="/inTheaters" class="v-nav">正在热映</router-link>
-      <router-link to="/comingSoon" class="v-nav">即将上映</router-link>
-    </div>
-    <div>
-      
-    </div>
-  </div>
+   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        query: ''
-      }
-    },
-    methods: {
-      serch: function () {
-        this.$router.push({path: '/searchPage'})
-      }
-    }
-  }
+export default {
+
+}
 </script>
 
 <style>
-  .v-header {
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    height: 60px;
-    padding: 10px 20px;
-    box-sizing: border-box;
-  }
-  .v-header-logo {
-    flex: 1;
-  }
-  .v-header-logo > div {
-    display: inline-block;
-  }
-  .v-header-logo > div:first-child img{
-    height: 42px;
-    width: 42px;
-  }
-  .v-header-logo > div:last-child {
-    vertical-align: top;
-  }
-  .v-header-input {
-    width: 100px;
-    line-height: 100%;
-  }
-  .v-load-title h3 {
-    font-size: 17px;
-    font-weight: 700;
-    color: #333;
-  }
-  .v-load-title div {
-    font-size: 12px;
-    color: #999;
-  }
-  .v-nav-bar {
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    background-color: #df2d2d;
-  }
-  .v-nav {
-    flex: 1;
-    text-align: center;
-    text-decoration: none;
-    height: 35px;
-    line-height: 35px;
-    font-size: 20px;
-    color: #F9FAFC;
-  }
-  .v-inp {
-    width: 100%;
-    height: 40px;
-    border-style: none;
-    border-radius: 15px;
-    padding-left: 10px;
-  }
+  
 </style>
